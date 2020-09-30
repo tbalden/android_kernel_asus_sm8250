2288b1e69910e49c10137eca5ad179f268076413
tbalden <illespal@gmail.com>
2020-09-30 16:07:15 -0700

    build.sh: generate bin2hex binaries

--- build.sh
+++ build.sh
@@ -24,6 +24,11 @@ export CROSS_COMPILE=$BASE_PATH/aarch64-linux-android-4.9/bin/aarch64-linux-andr
 export CROSS_COMPILE_ARM32=$BASE_PATH/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export LD_LIBRARY_PATH=$BASE_PATH/clang-10/lib:$LD_LIBRARY_PATH
 
+echo "Generating binary conversions"
+cd binaries
+./convert
+cd ..
+
 echo
 echo "Set DEFCONFIG"
 echo 

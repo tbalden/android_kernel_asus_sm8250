d6929f70ab5f28758d8f3a723d120956588a358c
tbalden <illespal@gmail.com>
2020-10-04 14:48:42 -0700

    binaries: overlay.sh: uci - add dmesg to non root accessible folder
    On ROG 3 to /dev/dmesg, and set system selinux context

--- binaries/overlay.sh
+++ binaries/overlay.sh
@@ -8,6 +8,10 @@ touch /dev/hosts_k
 chmod 644 /dev/hosts_k
 chcon u:object_r:system_file:s0 /dev/hosts_k
 
+#mv /data/local/tmp/dmesg /data/local/tmp/dmesg-old
+/system/bin/dmesg > /dev/dmesg
+chcon u:object_r:shell_data_file:s0 /dev/dmesg
+
 #touch /storage/emulated/0/hosts_k
 #chcon u:object_r:system_file:s0 /storage/emulated/0/hosts_k
 

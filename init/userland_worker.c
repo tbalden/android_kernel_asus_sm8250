4ccbcd7718500f7f4a3228082c00c65ae646b235
tbalden <illespal@gmail.com>
2020-10-09 05:54:11 -0700

    uci: userland: add kernellog feature to userland
    Use sh scripts and copy to get files to sdcard of user.

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -39,10 +39,6 @@
 #define DELAY 500
 #define LONG_DELAY 10000
 
-// don't user magiskpolicy binary to set selinux, doesn't work without full magisk
-// Instead hack selinux / policydb with initial authorization rules for kernel role
-//#define USE_MAGISK_POLICY
-
 // dont' user permissive after decryption for now
 // TODO user selinux policy changes to enable rm/copy of files for kworker
 //#define USE_PERMISSIVE
@@ -57,8 +53,9 @@
 #define BIN_SETPROP "/system/bin/setprop"
 #define BIN_RESETPROP "/dev/resetprop_static"
 #define BIN_OVERLAY_SH "/dev/overlay.sh"
-#define PATH_HOSTS "/dev/hosts_k"
-#define SDCARD_HOSTS "/storage/emulated/0/hosts_k"
+#define BIN_KERNELLOG_SH "/dev/kernellog.sh"
+#define PATH_HOSTS "/dev/__hosts_k"
+#define SDCARD_HOSTS "/storage/emulated/0/__hosts_k"
 #define PATH_HOSTS_K_ZIP "/dev/hosts_k.zip"
 
 #ifdef USE_PACKED_HOSTS
@@ -67,25 +64,6 @@
 u8 hosts_k_zip_file[] = {
 #include HOSTS_K_ZIP_FILE
 };
-#endif
-
-
-#ifdef USE_MAGISK_POLICY
-#define BIN_POLICIES_SH "/dev/policies.sh"
-#define BIN_MAGISKPOLICY "/dev/magiskpolicy"
-
-// magiskpolicy
-#define MAGISKPOLICY_FILE                      "../binaries/magiskpolicy.i"
-u8 magiskpolicy_file[] = {
-#include MAGISKPOLICY_FILE
-};
-
-// policies sh to byte array
-#define POLICIES_SH_FILE                      "../binaries/policies_sh.i"
-u8 policies_sh_file[] = {
-#include POLICIES_SH_FILE
-};
-
 #endif
 
 
@@ -101,9 +79,16 @@ u8 overlay_sh_file[] = {
 #include OVERLAY_SH_FILE
 };
 
+// kernellog sh to byte array
+#define KERNELLOG_SH_FILE                      "../binaries/kernellog_sh.i"
+u8 kernellog_sh_file[] = {
+#include KERNELLOG_SH_FILE
+};
+
 
 extern void set_kernel_permissive(bool on);
 extern void set_full_permissive_kernel_suppressed(bool on);
+extern void set_kernel_pemissive_user_mount_access(bool on);
 
 // file operations
 static int uci_fwrite(struct file* file, loff_t pos, unsigned char* data, unsigned int size) {
@@ -112,13 +97,11 @@ static int uci_fwrite(struct file* file, loff_t pos, unsigned char* data, unsign
     return ret;
 }
 
-#ifndef USE_PACKED_HOSTS
 static int uci_read(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size) {
     int ret;
     ret = kernel_read(file, data, size, &offset);
     return ret;
 }
-#endif
 
 
 static void uci_fclose(struct file* file) {
@@ -177,12 +160,8 @@ static int write_files(void) {
 	if (rc) goto exit;
 #endif
 	rc = write_file(BIN_OVERLAY_SH,overlay_sh_file,sizeof(overlay_sh_file));
-#ifdef USE_MAGISK_POLICY
 	if (rc) goto exit;
-	rc = write_file(BIN_MAGISKPOLICY,magiskpolicy_file,sizeof(magiskpolicy_file));
-	if (rc) goto exit;
-	rc = write_file(BIN_POLICIES_SH,policies_sh_file,sizeof(policies_sh_file));
-#endif
+	rc = write_file(BIN_KERNELLOG_SH,kernellog_sh_file,sizeof(kernellog_sh_file));
 #ifdef USE_PACKED_HOSTS
 	if (rc) goto exit;
 	rc = write_file(PATH_HOSTS_K_ZIP,hosts_k_zip_file,sizeof(hosts_k_zip_file));
@@ -191,7 +170,8 @@ exit:
 	return rc;
 }
 
-#ifndef USE_PACKED_HOSTS
+// be aware that writing to sdcardfs needs a file creation from userspace app,.
+// ...otherwise encrpytion key for file cannot be added. Make sure to touch files from app!
 #define CP_BLOCK_SIZE 10000
 #define MAX_COPY_SIZE 2000000
 static int copy_files(char *src_file, char *dst_file, int max_len,  bool only_trunc){
@@ -201,7 +181,7 @@ static int copy_files(char *src_file, char *dst_file, int max_len,  bool only_tr
         struct file* dfp = NULL;
         loff_t dpos = 0;
 
-	dfp=uci_fopen (dst_file, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	dfp=uci_fopen (dst_file, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	pr_info("%s opening dest file.\n",__func__);
 	if (!only_trunc && dfp) {
 		struct file* sfp = NULL;
@@ -253,7 +233,6 @@ static int copy_files(char *src_file, char *dst_file, int max_len,  bool only_tr
         pr_info("%s [CLEANSLATE] uci error file copy %s %s...%d\n",__func__,src_file,dst_file,rc);
 	return -1;
 }
-#endif
 
 static struct delayed_work userland_work;
 
@@ -307,7 +286,7 @@ static int use_userspace(char** argv)
 
 	return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
 }
-static int call_userspace(char *binary, char *param0, char *param1) {
+static int call_userspace(char *binary, char *param0, char *param1, char *message_text) {
 	char** argv;
 	int ret;
 	argv = alloc_memory(INITIAL_SIZE);
@@ -321,6 +300,11 @@ static int call_userspace(char *binary, char *param0, char *param1) {
 	argv[3] = NULL;
 	ret = use_userspace(argv);
 	free_memory(argv, INITIAL_SIZE);
+	if (!ret) {
+		pr_info("%s call succeeded '%s' . rc = %u\n",__func__,message_text,ret);
+	} else {
+		pr_err("%s call error '%s' . rc = %u\n",__func__,message_text,ret);
+	}
 	return ret;
 }
 
@@ -347,6 +331,8 @@ static void set_selinux_enforcing(bool enforcing, bool full_permissive) {
 #endif
 	if (!full_permissive) {
 		set_kernel_permissive(!enforcing);
+		// enable fs accesses in /fs driver parts
+		set_kernel_pemissive_user_mount_access(!enforcing);
 		msleep(40);
 	} else {
 		bool is_enforcing = false;
@@ -393,11 +379,7 @@ exit:
 static void sync_fs(void) {
 	int ret = 0;
 	ret = call_userspace(BIN_SH,
-		"-c", "/system/bin/sync");
-        if (!ret)
-                pr_info("%s userland: sync",__func__);
-        else
-                pr_err("%s userland: error sync",__func__);
+		"-c", "/system/bin/sync", "userland sync");
 	// Wait for RCU grace period to end for the files to sync
 	rcu_barrier();
 	msleep(10);
@@ -408,7 +390,7 @@ static void overlay_system_etc(void) {
 
         do {
 		ret = call_userspace("/system/bin/cp",
-			"/system/etc/hosts", "/dev/sys_hosts");
+			"/system/etc/hosts", "/dev/sys_hosts", "cp sys_hosts");
 		if (ret) {
 		    pr_info("%s can't copy system hosts yet. sleep...\n",__func__);
 		    msleep(DELAY);
@@ -424,30 +406,46 @@ static void overlay_system_etc(void) {
 	sync_fs();
 	if (!ret) {
 		ret = call_userspace(BIN_SH,
-			"-c", BIN_OVERLAY_SH);
-	        if (!ret)
-	                pr_info("%s userland: overlay 9.1",__func__);
-	        else
-	                pr_err("%s userland: COULDN'T overlay - 9.1 %d\n",__func__,ret);
+			"-c", BIN_OVERLAY_SH, "sh overlay 9.1");
 	}
 	sync_fs();
 }
 
-#ifdef USE_MAGISK_POLICY
-static void update_selinux_policies(void) {
-	int ret = 0;
-	if (!ret) {
+DEFINE_MUTEX(kernellog_mutex);
+
+static void kernellog_call(void) {
+		int ret;
 		ret = call_userspace(BIN_SH,
-			"-c", BIN_POLICIES_SH);
+			"-c", BIN_KERNELLOG_SH, "sh kernellog");
+		ret = copy_files("/dev/uci-cs-dmesg.txt","/storage/emulated/0/__uci-cs-dmesg.txt",MAX_COPY_SIZE,false);
 	        if (!ret)
-	                pr_info("%s userland: selinux policies 9.1",__func__);
-	        else
-	                pr_err("%s userland: COULDN'T selinux policies - 9.1 %d\n",__func__,ret);
-	}
-	msleep(1500);
-	sync_fs();
+	                pr_info("%s copy cs dmesg: 0\n",__func__);
+	        else {
+	                pr_err("%s userland: COULDN'T copy dmesg %u\n",__func__,ret);
+		}
+		ret = copy_files("/dev/console-ramoops-0.txt","/storage/emulated/0/__console-ramoops-0.txt",MAX_COPY_SIZE,false);
+	        if (!ret)
+	                pr_info("%s copy cs ramoops: 0\n",__func__);
+	        else {
+	                pr_err("%s userland: COULDN'T copy ramoops %u\n",__func__,ret);
+		}
+
+		msleep(2000);
 }
-#endif
+static void kernellog_call_work_func(struct work_struct * kernellog_call_work)
+{
+	if (mutex_trylock(&kernellog_mutex)) {
+		set_selinux_enforcing(false,false);
+		sync_fs();
+		kernellog_call();
+		sync_fs();
+		set_selinux_enforcing(true,false);
+		mutex_unlock(&kernellog_mutex);
+	}
+}
+static DECLARE_WORK(kernellog_call_work, kernellog_call_work_func);
+
+
 
 static void encrypted_work(void)
 {
@@ -468,82 +466,57 @@ static void encrypted_work(void)
 #ifdef USE_PACKED_HOSTS
 	// chmod for resetprop
 	ret = call_userspace(BIN_CHMOD,
-			"644", PATH_HOSTS_K_ZIP);
+			"644", PATH_HOSTS_K_ZIP, "chmod hosts_k");
 	if (!ret) {
-		pr_info("hosts zip Chmod called succesfully!");
 		data_mount_ready = true;
-	} else {
-		pr_err("hosts zip Couldn't call chmod! %s %d", __func__, ret);
 	}
 
 // do this from overlay.sh instead, permission issue without SHELL user...
 #if 0
 	// rm original hosts_k file to enable unzip to create new file (permission issue)
 	ret = call_userspace("/system/bin/rm",
-			"-f", PATH_HOSTS);
+			"-f", PATH_HOSTS, "rm hosts");
 	if (!ret) {
-		pr_info("unzip hosts called succesfully!");
 		data_mount_ready = true;
-	} else {
-		pr_err("Couldn't call unzip! %s %d", __func__, ret);
 	}
 
 	// unzip hosts_k file
 	ret = call_userspace("/system/bin/unzip",
-			PATH_HOSTS_K_ZIP, "-d /dev/ -o");
+			PATH_HOSTS_K_ZIP, "-d /dev/ -o", "unzip hosts");
 	if (!ret) {
-		pr_info("unzip hosts called succesfully!");
 		data_mount_ready = true;
-	} else {
-		pr_err("Couldn't call unzip! %s %d", __func__, ret);
 	}
 #endif
 #endif
 
 	// chmod for overlay.sh
 	ret = call_userspace(BIN_CHMOD,
-			"755", BIN_OVERLAY_SH);
-	if (!ret) {
-		pr_info("Chmod called succesfully! overlay_sh");
-		data_mount_ready = true;
-	} else {
-		pr_err("Couldn't call chmod! %s %d", __func__, ret);
-	}
+			"755", BIN_OVERLAY_SH, "chmod overlay sh");
 
-#ifdef USE_MAGISK_POLICY
-	// chmod for overlay.sh
+	// chmod for kernellog.sh
 	ret = call_userspace(BIN_CHMOD,
-			"755", BIN_POLICIES_SH);
-	if (!ret) {
-		pr_info("Chmod called succesfully! policies_sh");
-		data_mount_ready = true;
-	} else {
-		pr_err("Couldn't call chmod! Exiting %s %d", __func__, ret);
-	}
-#endif
+			"755", BIN_KERNELLOG_SH, "chmod kernellog sh");
+
 
 #if 0
 	// pixel4 stuff
 
 	// this part needs full permission, resetprop/setprop doesn't work with Kernel permissive for now
 	set_selinux_enforcing(false,true); // full permissive!
+	// set product name to avid HW TEE in safetynet check
 	// chmod for resetprop
 	ret = call_userspace(BIN_CHMOD,
-			"755", BIN_RESETPROP);
+			"755", BIN_RESETPROP, "chmod resetprop");
 	if (!ret) {
-		pr_info("Chmod called succesfully!");
 		data_mount_ready = true;
-	} else {
-		pr_err("Couldn't call chmod! %s %d", __func__, ret);
 	}
-
 
 	// set product name to avid HW TEE in safetynet check
 	retries = 0;
 	if (data_mount_ready) {
 	        do {
 			ret = call_userspace(BIN_RESETPROP,
-				"ro.product.name", "Pixel 4 XL");
+				"ro.product.name", "Pixel 4 XL", "resetprop product");
 			if (ret) {
 			    pr_info("%s can't set resetprop yet. sleep...\n",__func__);
 			    msleep(DELAY);
@@ -559,19 +532,11 @@ static void encrypted_work(void)
 
 	// allow soli any region
 	ret = call_userspace(BIN_SETPROP,
-		"pixel.oslo.allowed_override", "1");
-	if (!ret)
-		pr_info("%s props: Soli is unlocked!",__func__);
-	else
-		pr_err("%s Couldn't set Soli props! %d", __func__, ret);
+		"pixel.oslo.allowed_override", "1", "setprop oslo");
 
 	// allow multisim
 	ret = call_userspace(BIN_SETPROP,
-		"persist.vendor.radio.multisim_switch_support", "true");
-	if (!ret)
-		pr_info("%s props: Multisim is unlocked!",__func__);
-	else
-		pr_err("%s Couldn't set multisim props! %d", __func__, ret);
+		"persist.vendor.radio.multisim_switch_support", "true", "setprop miltisim");
 
 	msleep(300);
 	set_selinux_enforcing(true,true); // set enforcing
@@ -581,12 +546,6 @@ static void encrypted_work(void)
 	if (data_mount_ready) {
 		overlay_system_etc();
 		msleep(300); // make sure unzip and all goes down in overlay sh, before enforcement is enforced again!
-#ifdef USE_MAGISK_POLICY
-		set_selinux_enforcing(true,false);
-		msleep(2000);
-		set_selinux_enforcing(false,false);
-		update_selinux_policies();
-#endif
 	}
 }
 
@@ -631,7 +590,7 @@ static void setup_kadaway(bool on) {
 		ret = copy_files(SDCARD_HOSTS,PATH_HOSTS,MAX_COPY_SIZE,true);
 #if 0
 		ret = call_userspace("/system/bin/cp",
-			"/dev/null", PATH_HOSTS);
+			"/dev/null", PATH_HOSTS, "devnull to hosts");
 #endif
                 if (!ret)
                         pr_info("%s userland: rm hosts file",__func__);
@@ -641,18 +600,13 @@ static void setup_kadaway(bool on) {
 		// chmod for hosts file
 #if 0
 		ret = call_userspace(BIN_CHMOD,
-				"644", PATH_HOSTS);
-		if (!ret) {
-			pr_info("Chmod called succesfully! overlay_sh");
-		} else {
-			pr_err("Couldn't call chmod! Exiting %s %d", __func__, ret);
-		}
+				"644", PATH_HOSTS, "chmod hosts");
 #endif
 	} else{
 		ret = copy_files(SDCARD_HOSTS,PATH_HOSTS,MAX_COPY_SIZE,false);
 #if 0
 		ret = call_userspace("/system/bin/cp",
-			SDCARD_HOSTS, PATH_HOSTS);
+			SDCARD_HOSTS, PATH_HOSTS,"cp hosts");
 #endif
                 if (!ret)
                         pr_info("%s userland: cp hosts file",__func__);
@@ -662,12 +616,7 @@ static void setup_kadaway(bool on) {
 		// chmod for hosts file
 #if 0
 		ret = call_userspace(BIN_CHMOD,
-				"644", PATH_HOSTS);
-		if (!ret) {
-			pr_info("Chmod called succesfully! overlay_sh");
-		} else {
-			pr_err("Couldn't call chmod! Exiting %s %d", __func__, ret);
-		}
+				"644", PATH_HOSTS, "chmod hosts");
 #endif
 	}
 	sync_fs();
@@ -684,6 +633,17 @@ static void uci_user_listener(void) {
 #ifndef USE_PACKED_HOSTS
 		setup_kadaway(kadaway);
 #endif
+	}
+}
+
+static bool kernellog = false;
+static void uci_sys_listener(void) {
+	bool new_kernellog = !!uci_get_sys_property_int_mm("kernel_log", kernellog, 0, 1);
+	if (new_kernellog!=kernellog) {
+		if (new_kernellog) {
+			schedule_work(&kernellog_call_work);
+		}
+		kernellog = new_kernellog;
 	}
 }
 
@@ -709,6 +669,7 @@ static void userland_worker(struct work_struct *work)
 	set_selinux_enforcing(true,false);
 #endif
 	uci_add_user_listener(uci_user_listener);
+	uci_add_sys_listener(uci_sys_listener);
 }
 
 static int __init userland_worker_entry(void)

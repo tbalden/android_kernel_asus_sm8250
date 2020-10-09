6ae642952778c4fd2d85ab3225f52e2d8cf5e96e
tbalden <illespal@gmail.com>
2020-10-09 05:48:14 -0700

    binaries: move hosts_k to __hosts_k spare strcmp performance for filenames

--- binaries/kernellog.sh
+++ binaries/kernellog.sh
@@ -1,5 +1,14 @@
 #!/system/bin/sh
 
-/system/bin/dmesg > /storage/emulated/0/__uci-cs-dmesg.txt
-cp /sys/fs/pstore/console-ramoops-0 /storage/emulated/0/__console-ramoops-0.txt
+/system/bin/dmesg > /dev/uci-cs-dmesg.txt
+chcon u:object_r:shell_data_file:s0 /dev/uci-cs-dmesg.txt
+#cat /dev/uci-cs-dmesg.txt > /storage/emulated/0/__uci-cs-dmesg.txt
+touch /storage/emulated/0/__uci-cs-dmesg.txt
+
+cat /sys/fs/pstore/console-ramoops-0 > /dev/console-ramoops-0.txt
+chcon u:object_r:shell_data_file:s0 /dev/console-ramoops-0.txt
+#cat /dev/console-ramoops-0.txt > /storage/emulated/0/__console-ramoops-0.txt
+touch /storage/emulated/0/__console-ramoops-0.txt
+
+
 

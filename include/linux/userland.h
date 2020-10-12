e71ce0a3db5b9cfda256638d3707fd4a262df0d9
tbalden <illespal@gmail.com>
2020-10-12 01:55:31 -0700

    userland: selinux: move set enforce to selinux driver

--- include/linux/userland.h
+++ include/linux/userland.h
@@ -8,3 +8,6 @@ extern struct selinux_state *get_extern_state(void);
 extern struct selinux_state *extern_state;
 extern bool is_decrypted;
 extern bool is_before_decryption;
+
+extern int get_enforce_value(void);
+extern void set_selinux(int value);

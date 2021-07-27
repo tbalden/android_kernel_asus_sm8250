78c25eae8c9520177b0ebfd7816d184d29693805
tbalden <illespal@gmail.com>
2021-07-27 12:44:39 -0700

    build script: update for a11

--- build.sh
+++ build.sh
@@ -16,13 +16,15 @@ export ARCH=arm64
 export SUBARCH=arm64
 BASE_PATH=/home/android/pixel
 EXT_UTILS=$BASE_PATH/wahoo-kernel-tools/bin
-export CLANG_PATH=$BASE_PATH/clang-11/bin
+export CLANG_PATH=$BASE_PATH/clang-r383902/bin
 export PATH=${CLANG_PATH}:${EXT_UTILS}:${PATH}
 export DTC_EXT=$EXT_UTILS/dtc-aosp
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=$BASE_PATH/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$BASE_PATH/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=$BASE_PATH/clang-11/lib:$LD_LIBRARY_PATH
+# export CROSS_COMPILE_ARM32=$BASE_PATH/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+# export LD_LIBRARY_PATH=$BASE_PATH/clang-10/lib:$LD_LIBRARY_PATH
+
+#export ASUS_BUILD_PROJECT=ZS661KS
 
 echo "Generating binary conversions"
 cd binaries
@@ -33,6 +35,7 @@ echo
 echo "Set DEFCONFIG"
 echo 
 make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out vendor/cleanslate_obiwan_defconfig
+#cleanslate_defconfig
 
 echo
 echo "Build The Good Stuff"
